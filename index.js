6df74e80101763ac3f8be27847b8f33696e01918
AsadFahimTEC <asaduzzamanfahim838@gmail.com>
2024-06-08 14:42:08 +0600

    news add functionality finished properly

--- index.js
+++ index.js
@@ -24,9 +24,57 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
+    const newsInfocollection = client.db("Akash-Today").collection("newsInfo");
 
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
+
+     /////////////////////////////////////////////////////////////////////////
+    //                        news info part
+    ////////////////////////////////////////////////////////////////////////
+
+    app.post("/news",  async (req, res) => {
+      const data = req.body;
+      const result = await newsInfocollection.insertOne(data);
+      res.send(result);
+    });
+
+    app.get("/news", async (req, res) => {
+      const result = await newsInfocollection.find().toArray();
+      res.send(result);
+    });
+
+    app.get("/news/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const result = await newsInfocollection.findOne(filter);
+      res.send(result);
+    });
+
+    app.patch("/news/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const body = req.body;
+      const updatedoc = {
+        $set: {
+          title: body.title,
+          description: body.description,
+          photo: body.photo,
+          category: body.category,
+          date: body.date,
+        },
+      };
+      const result = await newsInfocollection.updateOne(filter, updatedoc);
+      res.send(result);
+    });
+
+    app.delete("/news/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const result = await newsInfocollection.deleteOne(filter);
+      res.send(result);
+    });
+
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
