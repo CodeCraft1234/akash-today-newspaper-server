bc1c4a0ad4543911b95b45e1624953884ad7bd44
AsadFahimTEC <asaduzzamanfahim838@gmail.com>
2024-07-07 15:49:37 +0600

    news added

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 require("dotenv").config();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5001;
 //MIADLEWERE
 app.use(express.json());
 app.use(cors())
